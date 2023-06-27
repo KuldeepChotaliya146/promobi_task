4b8454fc0c5b2351a5b90eb057b96ad3ac0dff2c
Kuldeep Chotaliya <kuldeepchotaliya17@gmail.com>
2023-06-27 09:23:25 +0530

    Added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+course_names = ["Javascript", "Data science"]
+course_names.each do |course|
+  course = Course.find_or_create_by(name: course)
+  2.times do
+    course.tutors.find_or_create_by(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+  end
+end
